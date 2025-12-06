03125cd0b011a33ce7b063f78c758ff5ff1aa3ba
Meelah09 <meelah12.sev@gmail.com>
2025-12-06 20:25:34 +0700

    Implement pricing and discount calculations in Lab206
    Added functionality to calculate total price, net price with VAT, discount, final price, and change.

--- Lab206.java
+++ Lab206.java
@@ -3,11 +3,36 @@ import java.text.DecimalFormat;
 
 public class Lab206 {
     public static void main(String[] args) {
-        final double price =299;
-        final double net =0.07;
+
+        final double price = 299.0;
+        final double vat = 0.07;
         DecimalFormat frm = new DecimalFormat("#,###.00");
 
-        int customer =Integer.parseInt(JOptionPane.showInputDialog("How many customer?"));
+        int customer = Integer.parseInt(JOptionPane.showInputDialog("How many customer?"));
 
+        double total = price * customer;
+
+        double net = total + (total * vat);
+
+        String msg1 = "Price with NET is " + frm.format(net) + " baht.\n"
+                + "How much of discount (%) on your coupon?";
+        double discount = Double.parseDouble(JOptionPane.showInputDialog(msg1));
+
+        double finalPrice = net - (net * discount / 100);
+
+        String msg2 = "Total price is " + frm.format(finalPrice) + " baht.\n"
+                + "Enter the amount the customer paid:";
+        double paid = Double.parseDouble(JOptionPane.showInputDialog(msg2));
+
+
+        double change = paid - finalPrice;
+
+
+        String msg3 = "Total price is " + frm.format(finalPrice) + " baht.\n"
+                + "Customer paid " + frm.format(paid) + " baht.\n"
+                + "Get change " + frm.format(change) + " baht.";
+
+        JOptionPane.showMessageDialog(null, msg3);
     }
 }
+
