60529ccef4e846166af3f42e448c4617905f39fc
Meelah09 <meelah12.sev@gmail.com>
2025-12-08 23:25:20 +0700

    Implement number range and even number display
    Added a program to read two numbers and print the range and even numbers.

--- Main.java
+++ Main.java
@@ -20,3 +20,43 @@ class 301{
         
     }
 }
+
+import java.util.Scanner;
+
+class 305 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first  number: ");
+        int first = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int second = scanner.nextInt();
+
+        int start = first;
+        int end = second;
+
+        if (first > second) {
+            start = second;
+            end = first;
+        }
+
+
+        // แสดงผลลัพธ์ชุดแรก
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // ขึ้นบรรทัดใหม่
+
+        for (int i = start; i <= end; i++) {
+
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+
+    }
+}
+
