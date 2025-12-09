b4bfbba83110e4497e56274baa1a58ab17fdca62
Meelah09 <meelah12.sev@gmail.com>
2025-12-09 07:23:04 +0700

    Update L03.java

--- L03.java
+++ L03.java
@@ -209,4 +209,43 @@ class 302 {
     }
 }
 
+import java.util.Scanner;
+
+ class Lab303 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the value for X coordinate: ");
+        int X = scanner.nextInt();
+        
+        System.out.print("Enter the value for Y coordinate: ");
+        int Y = scanner.nextInt();
+
+        String position = "";
+
+        // 3. ตรวจสอบเงื่อนไข
+        if (X == 0 && Y == 0) {
+            position = "Origin";
+        } else if (X == 0) {
+            position = "Y-axis";
+        } else if (Y == 0) {
+            position = "X-axis";
+        } else if (X > 0 && Y > 0) {
+            position = "First quadrant";
+        } else if (X < 0 && Y > 0) {
+            position = "Second quadrant";
+        } else if (X < 0 && Y < 0) {
+            position = "Third quadrant";
+        } else { 
+            position = "Fourth quadrant";
+        }
+        
+        System.out.println("The coordinate point (" + X + "," + Y + ") lies in the " + position);
+
+        scanner.close();
+    }
+}
+
 
