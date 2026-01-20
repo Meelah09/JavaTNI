d1cdc9b27e03959b9a7359bc2f7f12aaa83d95ef
Meelah09 <meelah12.sev@gmail.com>
2026-01-20 13:46:45 +0700

    Add files via upload

---
+++ BookInventory.java
@@ -0,0 +1,7 @@
+public class BookInventory {
+    public static void main (String[] args){
+        Book[] books = new Book[3];
+        for (int i =0
+
+        }
+    }
---
+++ BorrowBookTest.java
@@ -0,0 +1,2 @@
+public class BorrowBookTest {
+}
