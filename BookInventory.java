df5d9be7a00e76714479d24881858eb76f9f14ec
Meelah09 <meelah12.sev@gmail.com>
2026-01-20 13:59:26 +0700

    Update BookInventory.java

--- BookInventory.java
+++ BookInventory.java
@@ -1,7 +1,14 @@
 public class BookInventory {
     public static void main (String[] args){
         Book[] books = new Book[3];
-        for (int i =0
+        for (int i =0; i < books.length;i++){
+            String title = JOptionPane.showInputDialog("Enter book title: ");
+            int total = Integer.parseInt(JOptionPane.showInputDialog("Enter the total number: "));
+
+            books[i] = new Book(title,total);
+        }
+
 
         }
     }
+
