93c4d6c7b2efa099bcd7d3c1871e244d26cd98d2
Meelah09 <meelah12.sev@gmail.com>
2026-02-23 15:08:07 +0700

    Delete Ex102.java

--- Ex102.java
+++
@@ -1,8 +0,0 @@
-public class Ex102 {
-    public static void main(String[] args) {
-        System.out.println("Hello, I am Kornkamol Saewiset");
-        System.out.println("\"TNI-108\" \'Object-Oriented Programmingr\'");
-        System.out.println("\tThis is my first time learning Java");
-        System.out.println("\tI hope to get grade \"A\" ");
-    }
-}
